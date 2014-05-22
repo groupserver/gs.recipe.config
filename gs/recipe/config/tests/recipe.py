6301375a3974b08feac9735c1185ec8b2e27a09f
Michael JasonSmith <mpj17@onlinegroups.net>
2014-05-21 17:36:02 -0700

    Adding the setup for the recipe test

--- gs/recipe/config/tests/recipe.py
+++ gs/recipe/config/tests/recipe.py
@@ -13,8 +13,45 @@
 #
 ##############################################################################
 from __future__ import absolute_import, unicode_literals
+from mock import MagicMock
+import os
+from shutil import rmtree
+from tempfile import mkdtemp
 from unittest import TestCase
+# from zc.buildout import UserError
+import gs.recipe.config.recipe
+UTF8 = 'utf-8'
 
 
 class TestRecipe(TestCase):
     'Test the ConfigRecipe class'
+    def setUp(self):
+        self.tempdir = mkdtemp()
+        self.bindir = os.path.join(self.tempdir, 'bin')
+        os.mkdir(self.bindir)
+        vardir = os.path.join(self.tempdir, 'var')
+        os.mkdir(vardir)
+
+        self.buildout = {'buildout': {'directory': self.tempdir,
+                                        'bin-directory': self.bindir, }, }
+        self.name = 'postfix'
+        self.options = {}
+        self.options['recipe'] = 'gs.recipe.config'
+        self.options['database_username'] = 'example_db_user'
+        self.options['database_password'] = 'example_db_password'
+        self.options['database_host'] = 'db.example.com'
+        self.options['database_port'] = '5433'
+        self.options['database_name'] = 'example_db'
+        self.options['smtp_host'] = 'smtp.example.com'
+        self.options['smtp_port'] = '2525'
+        self.options['smtp_user'] = ''
+        self.options['smtp_password'] = ''
+
+        self.recipe = gs.recipe.config.recipe.ConfigRecipe(self.buildout,
+                                                    self.name, self.options)
+
+        gs.recipe.config.recipe.sys.stdout = MagicMock()
+        gs.recipe.config.recipe.sys.stderr = MagicMock()
+
+    def tearDown(self):
+        rmtree(self.tempdir)
