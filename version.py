39854504a5dd08c5482e3b6492b57ea51b3aca2b
Michael JasonSmith <mpj17@onlinegroups.net>
2014-06-27 16:02:24 -0700

    Upping the version number

--- version.py
+++ version.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
-version = '2.1.0'
-release = True
+version = '2.1.1'
+release = False
 
 #-----------------------------------------------------------------------------#
 import sys
